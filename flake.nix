748fa68a883f83dd2d5679563243b008d3249fbc
Luca Di Carlo <luca@dicarlo.email>
2024-01-16 07:07:40 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -1,111 +1,53 @@
 {
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+
   };
+  description = "Build image";
+  outputs = { self, nixpkgs, nixos-hardware }:
+    let
+      system = "x86_64-linux";
+      x86_64pkgs = nixpkgs.legacyPackages.${system};
 
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-    lib = nixpkgs.lib;
-  in rec {
-    devShell.${system} = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        rsync
-        zstd
-        git
-      ];
-    };
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
-
-    nixosConfigurations.m1 = lib.nixosSystem {
-      system = "aarch64-linux";
-
-      modules = [
-        ({
-          pkgs,
-          config,
-          ...
-        }: {
-          imports = [
-            ./kboot-conf
-            # "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
-            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+      aarch64system = "aarch64-linux";
+      aarch64pkgs = nixpkgs.legacyPackages.${aarch64system};
+    in
+    rec {
+      nixosConfigurations.odroid-m1s = nixpkgs.lib.nixosSystem
+        {
+          system.boot.kernelPackages = aarch64pkgs.linuxPackages_5_15;
+          system.stateVersion = "23.11";
+          # imports = [
+          # ];
+          modules = [
+            <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
+            # <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
+            # <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix>
+            # <nixpkgs/nixos/modules/installer/sd-card/sd-image-raspberrypi.nix>
+            nixos-hardware.nixosModules.hardkernel-odroid-hc4
+            {
+              #nixpkgs.crossSystem.system = aarch64system;
+              #  nixpkgs.config.allowUnsupportedSystem = true;
+              #  nixpkgs.hostPlatform.system = "armv7l-linux";
+              #  nixpkgs.buildPlatform.system = "x86_64-linux"; #If you build on x86 other wise changes this.
+              # ... extra configs as above
+              virtualisation.vmVariant = {
+                # following configuration is added only when building VM with build-vm
+                virtualisation = {
+                  memorySize = 2048; # Use 2048MiB memory.
+                  cores = 3;
+                  graphics = false;
+                };
+              };
+            }
           ];
 
-          sdImage = {
-            #compressImage = false;
-            populateFirmwareCommands = let
-              configTxt = pkgs.writeText "README" ''
-                Nothing to see here. This empty partition is here because I don't know how to turn its creation off.
-              '';
-            in ''
-              cp ${configTxt} firmware/README
-            '';
-            populateRootCommands = ''
-              ${config.boot.loader.kboot-conf.populateCmd} -c ${config.system.build.toplevel} -d ./files/kboot.conf
-            '';
-          };
-
-          #boot.loader.grub.enable = false;
-          boot.loader.kboot-conf.enable = true;
-          # Use kernel >6.6
-          boot.kernelPackages = pkgs.linuxPackages_latest;
-          # Stop ZFS breasking the build
-          boot.supportedFilesystems = lib.mkForce ["btrfs" "cifs" "f2fs" "jfs" "ntfs" "reiserfs" "vfat" "xfs"];
-
-          # I'm not completely sure if some of these could be omitted,
-          # but want to make sure disk access works
-          boot.initrd.availableKernelModules = [
-            "nvme"
-            "nvme-core"
-            "phy-rockchip-naneng-combphy"
-            "phy-rockchip-snps-pcie3"
-          ];
-          # Petitboot uses this port and baud rate on the boards serial port,
-          # it's probably good to keep the options same for the running
-          # kernel for serial console access to work well
-          boot.kernelParams = ["console=ttyS2,1500000"];
-          hardware.deviceTree.name = "rockchip/rk3568-odroid-m1.dtb";
-
-          # Enable nix flakes
-          nix.package = pkgs.nixFlakes;
-          nix.extraOptions = ''
-            experimental-features = nix-command flakes
-          '';
-          nix.nixPath = ["nixpkgs=${nixpkgs}"];
-
-          # includes this flake in the live iso : "/etc/nixcfg"
-          environment.etc.nixcfg.source =
-            builtins.filterSource
-            (path: type:
-              baseNameOf path
-              != ".git"
-              && type != "symlink"
-              && !(pkgs.lib.hasSuffix ".qcow2" path)
-              && baseNameOf path != "secrets")
-            ../.;
-
-    environment.systemPackages = [
-      pkgs.git #gotta have git
-    ];
-
-
-          services.openssh = {
-            enable = true;
-            settings.PermitRootLogin = "yes";
-          };
-          users.extraUsers.root.initialPassword = lib.mkForce "odroid";
-        })
-      ];
+        };
+      images.odroid-m1s = nixosConfigurations.odroid-m1s.config.system.build.sdImage;
+      devShells.x86_64-linux.default = x86_64pkgs.mkShell {
+        buildInputs = with x86_64pkgs;
+          [ zstd ];
+      };
     };
-
-    images = {
-      m1 = nixosConfigurations.m1.config.system.build.sdImage;
-    };
-  };
 }
