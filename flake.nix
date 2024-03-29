068b248c1f37f1bbc41f56ca0aac8af85b0a6835
Luca Di Carlo <luca@dicarlo.email>
2024-03-29 08:46:05 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -110,17 +110,21 @@
                 nix.extraOptions = ''
                   experimental-features = nix-command flakes
                 '';
-                # boot.kernelPackages = pkgs.linuxPackages_latest;
-                boot.kernelPackages = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_rchp);
+                boot.kernelPackages = pkgs.linuxPackages_latest;
+                boot.kernelPatches = [{
+                  name = "odroid-m1s-support";
+                  patch = kernel/0001-arm64-dts-rockchip-Add-Hardkernel-ODROID-M1S-board.patch;
+                }];
+                # boot.kernelPackages = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_rchp);
                 boot.supportedFilesystems = pkgs.lib.mkForce [ "btrfs" "cifs" "f2fs" "jfs" "ntfs" "reiserfs" "vfat" "xfs" ];
                 # system.boot.loader.kernelFile = "bzImage";
                 boot.kernelParams = [ "console=ttyS2,1500000" "debug" ];
-                boot.initrd.availableKernelModules = [
-                  "nvme"
-                  "nvme-core"
-                  "phy-rockchip-naneng-combphy"
-                  "phy-rockchip-snps-pcie3"
-                ];
+                # boot.initrd.availableKernelModules = [
+                #   "nvme"
+                #   "nvme-core"
+                #   "phy-rockchip-naneng-combphy"
+                #   "phy-rockchip-snps-pcie3"
+                # ];
                 hardware.deviceTree.enable = true;
                 hardware.deviceTree.name = "rockchip/rk3566-odroid-m1s.dtb";
                 # hardware.deviceTree.dtbSource = ./dtbs;
