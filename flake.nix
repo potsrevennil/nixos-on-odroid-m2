cde431960415282cb0ea7f89e0d1180f94b5a49e
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2025-06-25 15:02:43 +0800

    build: add picocom for serial port debugging

--- flake.nix
+++ flake.nix
@@ -144,7 +144,7 @@
         pkgs.mkShellNoCC {
           packages =
             builtins.attrValues {
-              inherit (pkgs) minicom screen
+              inherit (pkgs) picocom
                 nixpkgs-fmt
                 nixd
                 deadnix
