29d7dd4a736e4ef5d956b99368787a719543c9ef
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2025-07-01 02:31:38 +0800

    wip

--- flake.nix
+++ flake.nix
@@ -71,63 +71,13 @@
               ];
               nixpkgs.hostPlatform.system = system;
             }
-            (
-              { pkgs, ... }:
-              {
-                imports = [
-                  disko.nixosModules.disko
-                  "${nixos-hardware}/rockchip"
-                  "${nixos-hardware}/rockchip/disko.nix"
-                ];
-
-                nix = {
-                  package = pkgs.nixVersions.stable;
-                  nixPath = [ "nixpkgs=${nixpkgs}" ];
-                  extraOptions = ''
-                    experimental-features = nix-command flakes
-                  '';
-                };
-
-                boot = {
-                  kernelPackages = pkgs.linuxPackages_latest;
-                  supportedFilesystems = pkgs.lib.mkForce [ "btrfs" "vfat" "ext2" ];
-                  kernelParams = [ "debug" "console=ttyS2,1500000" ];
-                  initrd.availableKernelModules = [
-                    "nvme"
-                    "nvme-core"
-                  ];
-                };
-
-                hardware.deviceTree = {
-                  enable = true;
-                  name = "rockchip/rk3588s-odroid-m2.dtb";
-                  rockchip = {
-                    enable = true;
-                    platformFirmware = pkgs.lib.mkDefault uboot;
-                  };
-                };
-
-                system.stateVersion = "25.05";
-
-                networking.networkmanager.enable = true;
-
-                services.openssh = {
-                  enable = true;
-                  settings.PermitRootLogin = "yes";
-                };
-
-                users = {
-                  users.root = {
-                    extraGroups = [ "networkmanager" ];
-                    initialPassword = pkgs.lib.mkForce "odroid";
-                  };
-                };
-              }
-            )
+            (import ./odroid-m2-config.nix {
+              inherit pkgs nixos-hardware disko nixpkgs uboot;
+            })
           ];
         };
 
-      images.odroid-m2 = nixosConfigurations.odroid-m2.config.system.build.sdImage;
+      images.odroid-m2 = nixosConfigurations.odroid-m2.config.system.build.diskoImagesScript;
 
       packages = {
         aarch64-linux.default = packages.all;
